f9d02a87e9a9d0695dd05caefc8cec0045c812fd
Paku- <pawel@4x4.org.pl>
2013-12-23 15:35:31 +0100

    Bt Dev list moved to the ConnectivityFragment.java, items has onItemClick implemented now.

--- src/com/paku/mavlinkhub/communication/CommunicationHUB.java
+++ src/com/paku/mavlinkhub/communication/CommunicationHUB.java
@@ -1,18 +1,10 @@
 package com.paku.mavlinkhub.communication;
 
-import java.util.ArrayList;
-import java.util.Set;
-
-import com.paku.mavlinkhub.R;
-import com.paku.mavlinkhub.ui_helpers.ListView_BTDevices;
-
-import android.app.Activity;
 import android.app.Application;
 import android.bluetooth.BluetoothAdapter;
-import android.bluetooth.BluetoothDevice;
 import android.content.Context;
-import android.content.Intent;
-import android.widget.Toast;
+
+
 
 public class CommunicationHUB extends Application{
 	
@@ -28,39 +20,6 @@ public class CommunicationHUB extends Application{
 	}
 	
 	public void ConnectBT() {
-	
-		
-		mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
-		Set<BluetoothDevice> pairedDevices = mBluetoothAdapter.getBondedDevices();
-        final ArrayList<String> pairedDevicesView = new ArrayList<String>();
-		
-		if (!mBluetoothAdapter.isEnabled()) {
-			// No BT adapter or no paired devices.
-			Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
-			((Activity) appContext).startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);						
-		}
-
-		// If there are paired devices
-		if (pairedDevices.size() > 0) {
-		    // Loop through paired devices
-			pairedDevicesView.clear();
-		    for (BluetoothDevice device : pairedDevices) {
-				// Add the name and address to an array adapter to show in a ListView
-		    	pairedDevicesView.add(device.getName() + "\n" + device.getAddress());
-		    }
-		    
-		    
-            Intent intentBtSelect = new Intent();
-            intentBtSelect.setClass(appContext, ListView_BTDevices.class);
-            intentBtSelect.putExtra("BTDevList",pairedDevicesView.toArray(new String[0]));
-            ((Activity) appContext).startActivityForResult(intentBtSelect, 0);	            
-		    
-		    
-		}		
-		else{
-			Toast.makeText(appContext, R.string.error_no_bt_adapter_or_paired_devices,
-					   Toast.LENGTH_LONG).show();						
-		}
 					
 		
 	}
--- src/com/paku/mavlinkhub/fragments/ConnectivityFragment.java
+++ src/com/paku/mavlinkhub/fragments/ConnectivityFragment.java
@@ -1,16 +1,32 @@
 package com.paku.mavlinkhub.fragments;
 
+import java.util.ArrayList;
+import java.util.Set;
+
 import com.paku.mavlinkhub.R;
 import com.paku.mavlinkhub.communication.CommunicationHUB;
 
+
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
+import android.widget.Toast;
 
 public class ConnectivityFragment extends Fragment {
+
+	private BluetoothAdapter mBluetoothAdapter;
+	private static final int REQUEST_ENABLE_BT = 1;
+	ListView btDevListView;
+	Button button;
 
 	public ConnectivityFragment() {
 	}
@@ -22,9 +38,17 @@ public class ConnectivityFragment extends Fragment {
 		View connView = inflater.inflate(R.layout.fragment_connectivity,
 				container, false);
 
+		btDevListView = (ListView) connView.findViewById(R.id.list_bt_bonded);
+		button = (Button) connView.findViewById(R.id.button_connect);
 		
-		final Button button = (Button) connView
-				.findViewById(R.id.button_connect);
+
+		return connView;
+	}
+
+	@Override
+	public void onViewCreated(View view, Bundle savedInstanceState) {
+		super.onViewCreated(view, savedInstanceState);
+
 		button.setOnClickListener(new View.OnClickListener() {
 			public void onClick(View v) {
 				CommunicationHUB comHUB = (CommunicationHUB) getActivity()
@@ -32,8 +56,70 @@ public class ConnectivityFragment extends Fragment {
 				comHUB.ConnectBT();
 			}
 		});
+			
+		button.setVisibility(View.INVISIBLE);
+		
 
-		return connView;
+
 	}
 
+	@Override
+	public void onResume() {
+		super.onResume();
+
+		mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+		Set<BluetoothDevice> pairedDevices = mBluetoothAdapter
+				.getBondedDevices();
+		final ArrayList<String> bondedDevicesList = new ArrayList<String>();
+
+		if (!mBluetoothAdapter.isEnabled()) {
+			
+			Intent enableBtIntent = new Intent(
+					BluetoothAdapter.ACTION_REQUEST_ENABLE);
+			this.startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+		}
+
+		// If there are paired devices
+		if (pairedDevices.size() > 0) {
+			// Loop through paired devices
+			bondedDevicesList.clear();
+			for (BluetoothDevice device : pairedDevices) {
+				// Add the name and address to an array adapter to show in a
+				// btDevListView
+				bondedDevicesList.add(device.getName() + "\n"
+						+ device.getAddress());
+			}
+			
+								
+		    ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity().getApplicationContext(),
+		            android.R.layout.simple_list_item_1, bondedDevicesList.toArray(new String[0]));					
+			
+		    btDevListView.setAdapter(adapter);									
+			btDevListView.setOnItemClickListener(btListClickListener);
+			
+
+		}else{
+			
+			Toast.makeText(getActivity().getApplicationContext(), R.string.error_no_paired_bt_devices_found_pair_device_first,
+					   Toast.LENGTH_LONG).show();						
+			
+			
+		}
+
+	}
+
+	
+    private final AdapterView.OnItemClickListener btListClickListener = new AdapterView
+            .OnItemClickListener() {
+        @Override
+        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+        	button.setVisibility(View.VISIBLE);
+        	
+        }
+    };	
+	
+	
+	
+	
 }
