17bb7aaea2b1685d38e9870d67ef864949085b0a
Paku- <pawel@4x4.org.pl>
2013-12-23 18:29:58 +0100

    ListView colors added

--- src/com/paku/mavlinkhub/fragments/ConnectivityFragment.java
+++ src/com/paku/mavlinkhub/fragments/ConnectivityFragment.java
@@ -6,10 +6,10 @@ import java.util.Set;
 import com.paku.mavlinkhub.R;
 import com.paku.mavlinkhub.communication.CommunicationHUB;
 
-
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -19,6 +19,7 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class ConnectivityFragment extends Fragment {
@@ -40,7 +41,6 @@ public class ConnectivityFragment extends Fragment {
 
 		btDevListView = (ListView) connView.findViewById(R.id.list_bt_bonded);
 		button = (Button) connView.findViewById(R.id.button_connect);
-		
 
 		return connView;
 	}
@@ -56,10 +56,8 @@ public class ConnectivityFragment extends Fragment {
 				comHUB.ConnectBT();
 			}
 		});
-			
-		button.setVisibility(View.INVISIBLE);
-		
 
+		button.setVisibility(View.INVISIBLE);
 
 	}
 
@@ -73,7 +71,7 @@ public class ConnectivityFragment extends Fragment {
 		final ArrayList<String> bondedDevicesList = new ArrayList<String>();
 
 		if (!mBluetoothAdapter.isEnabled()) {
-			
+
 			Intent enableBtIntent = new Intent(
 					BluetoothAdapter.ACTION_REQUEST_ENABLE);
 			this.startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
@@ -89,37 +87,50 @@ public class ConnectivityFragment extends Fragment {
 				bondedDevicesList.add(device.getName() + "\n"
 						+ device.getAddress());
 			}
-			
-								
-		    ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity().getApplicationContext(),
-		            android.R.layout.simple_list_item_1, bondedDevicesList.toArray(new String[0]));					
-			
-		    btDevListView.setAdapter(adapter);									
-			btDevListView.setOnItemClickListener(btListClickListener);
-			
 
-		}else{
-			
-			Toast.makeText(getActivity().getApplicationContext(), R.string.error_no_paired_bt_devices_found_pair_device_first,
-					   Toast.LENGTH_LONG).show();						
-			
-			
+			ArrayAdapter<String> adapter = new ArrayAdapter<String>(
+					getActivity().getApplicationContext(),
+					android.R.layout.simple_list_item_1,
+					//android.R.layout.simple_dropdown_item_1line,
+					bondedDevicesList.toArray(new String[0])) {
+
+				@Override
+				public View getView(int position, View convertView,
+						ViewGroup parent) {
+					View view = super.getView(position, convertView, parent);
+
+					TextView textView = (TextView) view
+							.findViewById(android.R.id.text1);
+					textView.setTextColor(Color.DKGRAY);
+
+					return view;
+				}
+			}
+
+			;
+
+			btDevListView.setAdapter(adapter);
+			btDevListView.setOnItemClickListener(btListClickListener);
+
+		} else {
+
+			Toast.makeText(
+					getActivity().getApplicationContext(),
+					R.string.error_no_paired_bt_devices_found_pair_device_first,
+					Toast.LENGTH_LONG).show();
+
 		}
 
 	}
 
-	
-    private final AdapterView.OnItemClickListener btListClickListener = new AdapterView
-            .OnItemClickListener() {
-        @Override
-        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+	private final AdapterView.OnItemClickListener btListClickListener = new AdapterView.OnItemClickListener() {
+		@Override
+		public void onItemClick(AdapterView<?> parent, View view, int position,
+				long id) {
 
-        	button.setVisibility(View.VISIBLE);
-        	
-        }
-    };	
-	
-	
-	
-	
+			button.setVisibility(View.VISIBLE);
+
+		}
+	};
+
 }
